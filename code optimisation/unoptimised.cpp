e4861d5185262f53368e9206776053bff522a2ec
Shubs2002 <97461214+Shubs2002@users.noreply.github.com>
2023-04-26 20:53:05 +0530

    Add files via upload

---
+++ code optimisation/unoptimised.cpp
@@ -0,0 +1,39 @@
+// Code Optimisation
+
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+// Unoptimised 
+int main()
+{
+
+time_t start, end; 
+
+time(&start);
+
+int m =0, l=12;
+for(long int i=0; i<1000000000; i++){
+    m = m+1;
+
+}
+
+for(long int i=0; i<1000000000; i++){
+
+
+l = l + 1;
+
+} 
+time(&end);
+
+// Time taken by program is: 4.000000 sec in my pc
+
+// Calculating total time taken by the program. 
+double time_taken = double(end - start); 
+cout << "Time taken by program is :"<< fixed << time_taken << setprecision(2); cout << "sec " << endl;
+
+return 0;
+
+}
